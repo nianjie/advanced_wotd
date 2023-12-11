6c2598c742f769e9505103d7475f612be1072e2a
nj <4769034+nianjie@users.noreply.github.com>
2023-12-11 16:30:06 +0900

    start to build prototype of get-word module.

---
+++ prototype.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rss'
+require 'open-uri'
+
+url = 'https://merriam-webster.com/wotd/feed/rss2'
+
+def find_non_empty_props(item)
+  item.instance_variables.filter do |name|
+    value = item.instance_variable_get name
+    (value.respond_to? :empty?) ? !value&.empty? : value
+  end
+end
+
+non_empty_props = [:title, :link, :description, :encolsure, :pubDate, :itunes_summary, :itunes_duration]
+
+URI.open(url) do |rss|
+  feed = RSS::Parser.parse rss
+  props = find_non_empty_props(feed.items[0])
+  feed.items.each_index do |item, i|
+    puts "Item: #{i}"
+    props.each do |name|
+      puts "#{name} : #{item.instance_variable_get name}"
+    end
+    puts "-" * 20
+  end
+end
